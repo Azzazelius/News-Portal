7747892566820094ba2c0905b75f7de50d3816a5
Aleksandr Zhevnerov <azzazelius@gmail.com>
2023-04-10 18:04:46 +0300

    added censor filter for title and content

---
+++ news/templatetags/custom_filters.py
@@ -0,0 +1,17 @@
+from django import template
+
+
+register = template.Library()
+
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+
+bad_words = ["редиска", "Редиски", "петух гамбургской", "Новохудоноссор"]
+
+@register.filter()
+def censor(value):
+   for word in bad_words:
+      if word.lower() in value.lower():
+         value = value.replace(word[1:-2], '*' * (len(word)-1))
+   return value
+
