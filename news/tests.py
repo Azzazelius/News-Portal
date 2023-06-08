605e270932f356447f64ab10d4fa80ca90d37309
alex_z <azzazelius@gmail.com>
2023-06-08 20:23:26 +0300

    Add CSS

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.contrib.auth.models import User
+from django.db.models import Max
+from news.models import Author
+from news.models import Category
+from news.models import Post
+from news.models import Comment
+
